88a3a3144e8e1416fa9f79bb53383878b4801f21
jksaWork <100087939+jksaWork@users.noreply.github.com>
2022-04-05 04:03:08 -0700

    Credntial To Function

--- app/Helpers/SalaClass.php
+++ app/Helpers/SalaClass.php
@@ -7,9 +7,9 @@ class SalaClass {
     public function __construct()
     {
         $this->provider = new Salla([
-            'clientId'     => '{client-id}', // The client ID assigned to you by Salla
-            'clientSecret' => '{client-secret}', // The client password assigned to you by Salla
-            'redirectUri'  => 'https://yourservice.com/callback_url', // the url for current page in your service
+            'clientId'     => 'c5e26ae228c097732386852c0194ade7', // The client ID assigned to you by Salla
+            'clientSecret' => '470e3ce6a091ce4a43fe30be1792313c', // The client password assigned to you by Salla
+            'redirectUri'  => 'https://dev.faster.themsc.net', // the url for current page in your service
         ]);
     }
 }
