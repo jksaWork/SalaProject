14d140a340e5eca071a8a9e626116206119c4f65
jksaWork <100087939+jksaWork@users.noreply.github.com>
2022-04-05 04:41:07 -0700

    Update AuthTokenControoler.php

--- app/Http/Controllers/AuthTokenControoler.php
+++ app/Http/Controllers/AuthTokenControoler.php
@@ -15,7 +15,7 @@ class AuthTokenControoler extends Controller
         $provider = new Salla([
             'clientId'     => 'c5e26ae228c097732386852c0194ade7', // The client ID assigned to you by Salla
             'clientSecret' => '470e3ce6a091ce4a43fe30be1792313c', // The client password assigned to you by Salla
-            'redirectUri'  => 'https://dev.faster.themsc.net', // the url for current page in your service
+            'redirectUri'  => 'https://sala.themsc.net/webhock', // the url for current page in your service
         ]);
 
         if (!isset($_GET['code']) || empty($_GET['code'])) {
