2b835b2743be613ba7581528cf91a791aaeb7ca2
jksaWork <100087939+jksaWork@users.noreply.github.com>
2022-04-06 06:49:20 -0700

    hock2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthTokenControoler;
+use App\Http\Controllers\hock2;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
