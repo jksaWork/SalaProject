4bebc5cc2b94423eb588f2e24719d8dfcab426b2
jksaWork <100087939+jksaWork@users.noreply.github.com>
2022-04-06 04:35:33 -0700

    Nmae

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -6,11 +6,21 @@ echo "Deployment started ..."
 # Enter maintenance mode or return true
 # if already is in maintenance mode
 (php artisan down) || true
+# remove old env ----------------
+rm -rf .env
 
+echo 'old env has discard Successfuly'
 # Pull the latest version of the app
+git stash
 git config core.sshCommand 'ssh -i ~/.ssh/cpanel'
-git pull origin main
+git pull origin dev
 #commit
+echo 'Start Copy Env Vaibale'
+cp .env.example .env
+
+echo 'Filed Is Copyed'
+
+
 # Install composer dependencies
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
@@ -28,5 +38,8 @@ php artisan migrate --force
 
 # Exit maintenance mode
 php artisan up
+echo 'server is avaliable'
 
 echo "Deployment finished!"
+
+echo 'Deployment to DevLopemt Server Was Done'
