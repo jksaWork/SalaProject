8a532700db815ff6f321e9aacc1bcde5bc829e10
jksaWork <100087939+jksaWork@users.noreply.github.com>
2022-04-07 13:30:31 -0700

    Update GetProductFromSalePoint.php

--- app/Listeners/GetProductFromSalePoint.php
+++ app/Listeners/GetProductFromSalePoint.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use SoapClient;
 
-class GetProductFromSalePoint
+class GetProductFromSalePoint implements ShouldQueue
 {
     /**
      * Create the event listener.
