556c0ae557c6c52fa9327d87f58cb32c6fd1959b
jksaWork <100087939+jksaWork@users.noreply.github.com>
2022-04-06 17:01:18 -0700

    ret req

--- app/Http/Controllers/AuthTokenControoler.php
+++ app/Http/Controllers/AuthTokenControoler.php
@@ -14,6 +14,8 @@ class AuthTokenControoler extends Controller
     public function  getTokenWithCode(Request $req)
     {
         // info($req);
+
+        return $req;
         $provider = new Salla([
             'clientId'     => 'c5e26ae228c097732386852c0194ade7', // The client ID assigned to you by Salla
             'clientSecret' => '470e3ce6a091ce4a43fe30be1792313c', // The client password assigned to you by Salla
