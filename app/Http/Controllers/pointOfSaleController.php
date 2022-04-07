82f566c1300157987c3e52d61f0d1b5c8602f8a3
jksaWork <100087939+jksaWork@users.noreply.github.com>
2022-04-07 06:35:50 -0700

    t 01

--- app/Http/Controllers/pointOfSaleController.php
+++ app/Http/Controllers/pointOfSaleController.php
@@ -21,7 +21,7 @@ class pointOfSaleController extends Controller
             'signature' => $signature,
         );
 
-        $myXMLData = $client->__soapCall('POSGetProductIDRequest', array($params));
+        $myXMLData = $client->__soapCall('POSGetProductListRequest', array($params));
 
         echo "<pre>";
         echo var_dump($myXMLData);
