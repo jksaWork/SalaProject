71a5e99c2b9a21b40851aa987921f06f683b483f
jksaWork <100087939+jksaWork@users.noreply.github.com>
2022-04-06 18:32:50 -0700

    Update 2022_04_06_191311_create_products_table.php

--- database/migrations/2022_04_06_191311_create_products_table.php
+++ database/migrations/2022_04_06_191311_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id')->nullable();
+            $table->integer('client_id')->nullable();
             $table->string('name')->nullable();
             $table->string('sku')->nullable();
             $table->string('type')->nullable();
