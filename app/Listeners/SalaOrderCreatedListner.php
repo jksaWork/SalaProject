17d5e266d8c4ea85f2a006ebd6a2b9fd786b262b
jksaWork <100087939+jksaWork@users.noreply.github.com>
2022-04-09 14:49:40 -0700

    inside foreach

--- app/Listeners/SalaOrderCreatedListner.php
+++ app/Listeners/SalaOrderCreatedListner.php
@@ -28,27 +28,25 @@ class SalaOrderCreatedListner
     public function handle($event)
     {
         // dd('jksa');
-        $posUsername = 'info@dataked.com';
-        $secret = 'v35r#UhJgT$AJzN3BB';
-        $productCode ='3176';
-        $signature = md5($posUsername . $productCode .$secret);
-        $terminalId ='307598';
-        $trxRefNumber = $terminalId . "" . time();
-        $client = new SoapClient('https://www.ocstaging.net/webservice/OneCardPOSSystem.wsdl');
-
-        $params = array(
-        	'posUsername'=>$posUsername,
-        	'productCode'=>$productCode,
-        	'signature'=>$signature,
-        	'terminalId'=>$terminalId,
-        	'trxRefNumber'=>$trxRefNumber
-        	);
-        $myXMLData = $client->__soapCall('POSPurchaseProduct', array($params));
-        echo "<pre>";
-        echo var_dump($myXMLData);
-        echo "<pre>";
-
-
-
+        $FinalResponse = [];
+        for ($i=0; $i < 5 ; $i++) {
+            $posUsername = 'info@dataked.com';
+            $secret = 'v35r#UhJgT$AJzN3BB';
+            $productCode ='3176';
+            $signature = md5($posUsername . $productCode .$secret);
+            $terminalId ='307598';
+            $trxRefNumber = $terminalId . "" . time();
+            $client = new SoapClient('https://www.ocstaging.net/webservice/OneCardPOSSystem.wsdl');
+            $params = array(
+                'posUsername'=>$posUsername,
+                'productCode'=>$productCode,
+                'signature'=>$signature,
+                'terminalId'=>$terminalId,
+                'trxRefNumber'=>$trxRefNumber
+                );
+            $myXMLData = $client->__soapCall('POSPurchaseProduct', array($params));
+            $FinalResponse[] =  $myXMLData;
+        }
+        dd($FinalResponse);
     }
 }
