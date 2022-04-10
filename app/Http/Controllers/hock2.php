726a8dda2b0696944b519d39f8afd1f62462cba7
jksaWork <100087939+jksaWork@users.noreply.github.com>
2022-04-10 04:04:19 -0700

    Update hock2.php

--- app/Http/Controllers/hock2.php
+++ app/Http/Controllers/hock2.php
@@ -11,7 +11,7 @@ class hock2 extends Controller
     public function  hock2(Request $request)
     {
             if($request->event == 'product.created'){
-                info('product_created');
+                info('product_created'. $request);
             //     info($request->data['items']);
             //     info($request->data['items'][0]);
             //     event(new SalaOrderCreated($request->data['items'][0]['products'] ?? $request->data['items'][0]['product']));
