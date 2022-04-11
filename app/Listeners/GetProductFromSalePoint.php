200f0c862a081b10617239794dbf2a4cea818b7c
jksaWork <100087939+jksaWork@users.noreply.github.com>
2022-04-11 02:22:55 -0700

    Update GetProductFromSalePoint.php

--- app/Listeners/GetProductFromSalePoint.php
+++ app/Listeners/GetProductFromSalePoint.php
@@ -42,15 +42,6 @@ class GetProductFromSalePoint
             'signature' => $signature,
         );
         $response = $client->__soapCall('POSGetProductList', array($params));
-        info('before_line.44');
-        
-        info('before_line.46');
-        try
-        {
-            info("$response->productList");
-        }catch(Exception $e){
-
-        }
         info('before_line.48');
         $Products = $response->productList->product;
         info($Products);
