342223a7f9eb0f5aecf1be8516b022f27aa1d76e
Ahmed MacOs <ahmedict6@gmail.com>
2022-04-14 04:12:53 +0200

    use SoapClient;

--- app/Http/Controllers/GEtTables.php
+++ app/Http/Controllers/GEtTables.php
@@ -7,6 +7,7 @@ use App\Models\PointOfSaleEqualSalaProduct;
 use App\Models\PosProducts;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use SoapClient;
 
 class GEtTables extends Controller
 {
