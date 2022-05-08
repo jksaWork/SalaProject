43230c1dfcc281cfe4827842b7d202065abe6292
eslamgabany <eslam.gabany@yahoo.com>
2022-05-08 20:05:46 +0200

    Card & sala

--- app/Http/Controllers/GEtTables.php
+++ app/Http/Controllers/GEtTables.php
@@ -88,6 +88,7 @@ class GEtTables extends Controller
 
 
 
+
     public function Productprint()
     {
         $Products = DB::select("SELECT * from sala_products where  product_id  in( select sala_product_id from point_of_sale_equal_sala_products ) ");
